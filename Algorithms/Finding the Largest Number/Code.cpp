34cad16c81e6b92aa01082408761c1c34423fe09
Sarah Rosales <sarahgrosales@gmail.com>
2025-07-03 21:57:52 -0400

    Finding the Largest Number Code

--- Algorithms/Finding the Largest Number/Code.cpp
+++ Algorithms/Finding the Largest Number/Code.cpp
@@ -11,11 +11,7 @@ int main (){
    for (int i = 0; i < a; i++){ 
        // now 'i' equals 1 and 'j' equals 0; 'j' keeps adding 1 (0-5) then goes back to first loop
        for (int j = 0; j < a; j++){
-           // if 'j' = 0; n[8] > n[8] && 0 < n[8] (this repeats all numbers from 0-5 for 'j')
-           if (n[i] > n[j] && b < n[j]) {
-           b = n[i];
-           // else if 'j' = 0; n[8] > 0; which is true so it repeats 'j' for loop
-           } else if(n[j]>b) {
+           if (n[j]>b) {
            b = n[j];
        }
    }   
