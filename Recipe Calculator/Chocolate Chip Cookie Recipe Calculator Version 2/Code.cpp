34c08a99c3cd129e483ce6bd54dd7152e7e3f34c
Sarah Rosales <sarahgrosales@gmail.com>
2025-07-04 18:35:37 -0400

    Upgraded version of the recipe tracker

---
+++ Recipe Calculator/Chocolate Chip Cookie Recipe Calculator Version 2/Code.cpp
@@ -0,0 +1,7 @@
+// This second version of the cookie recipe calculator is 
+// meant to have several upgrades such as:
+//   1. Decimal to fraction conversion
+//   2. Measurement size conversion
+//   3. Extra prompt when 0 or other numbers are entered.
+//   4. Calorie tracker?
+
