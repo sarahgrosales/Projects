c110bc760cd9299d278df294ec60f695ab433a5c
Sarah Rosales <sarahgrosales@gmail.com>
2025-07-05 16:49:33 -0400

    Update Progressive_On_and_Off.cpp

--- Arduino/LED/Progressive On and Off/Progressive_On_and_Off.cpp
+++ Arduino/LED/Progressive On and Off/Progressive_On_and_Off.cpp
@@ -10,19 +10,19 @@ void setup()             // Happens only once, the arduino hunts for setup.
   { 
     pinMode(RED, OUTPUT);        // Tells the LED to Output. 
     pinMode(ORANGE, OUTPUT);     // Tells the LED to Output. 
-  	pinMode(YELLOW, OUTPUT);     // Tells the LED to Output. 
+    pinMode(YELLOW, OUTPUT);     // Tells the LED to Output. 
 }
 
 void loop(){                      // Starts after void setup.
 
-	for (int i = 0; i < 250; i++){        // This for loop progressively increases the brightness of the LED. (Max is 250)
-	    	analogWrite(RED, i);            // Tells the LED to be set to the brightness.
+    for (int i = 0; i < 250; i++){        // This for loop progressively increases the brightness of the LED. (Max is 250)
+	analogWrite(RED, i);            // Tells the LED to be set to the brightness.
       	analogWrite(ORANGE, i);         // Tells the LED to be set to the brightnes.
       	analogWrite(YELLOW, i);         // Tells the LED to be set to the brightness. 
         delay (wait);                   // Wait 10 miliseconds.
     }
     for (int i = 250; i > 0; i--){      // This for loop progressively decreases the brightness of the LED. 
-    	  analogWrite(RED, i);
+    	analogWrite(RED, i);
       	analogWrite(ORANGE, i);
       	analogWrite(YELLOW, i);
         delay (wait);
