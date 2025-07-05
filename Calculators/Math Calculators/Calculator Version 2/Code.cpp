b1f7fbb38506e357c0154c4e2a45a2e5b8d90ae5
Sarah Rosales <sarahgrosales@gmail.com>
2025-07-05 02:43:35 -0400

    Version 2

--- Calculators/Math Calculators/Calculator Version 2/Code.cpp
+++ Calculators/Math Calculators/Calculator Version 2/Code.cpp
@@ -4,17 +4,19 @@
 using namespace std;
 
 int main (){
-   int a;
-   int b;
-   int temp;
-   char c;
+   int a;        // Declares the a integer.
+   int b;        // Declares the b integer.
+   int temp;     // Declares the temp integer.
+   char c;       // Declared the c character.
+   
+   // What the user sees.
+   cout << "Enter the first number: ";       // The user is prompted to enter the first number.
+   cin >> a;                                 // The first number the user inputs is set to a.
+   cout << "Enter the second number: ";      // The user is prompted to enter the second number.
+   cin >> b;                                 // The first number the user inputs is set to b.
 
-   cout << "Enter the first number: ";
-   cin >> a;
-   cout << "Enter the second number: ";
-   cin >> b;
-
-   cout << "Select the operation you want: " << endl;
+   // Prompts the user to select what operation they want done.
+   cout << "Select the operation you want: " << endl;    
    cout << "       1. Addition" << endl;
    cout << "       2. Subtraction" << endl;
    cout << "       3. Multiplication" << endl;
@@ -22,37 +24,38 @@ int main (){
    cout << "       5. Modulus" << endl;
    cout << "       6. Power" << endl;
 
+   // Prompts the user to enter the operation they would like to do.
    cout << "Enter your choice: ";
    cin >> c;
 
-       switch (c)
+       switch (c)                                                      // Declares the switch function.
        {
-           case '1':
-               cout << "Your addition is: " << a + b << endl;
-               break;
-           case '2':
-               cout << "Your subtraction is: " << a - b << endl;
-               break;
-           case '3':
-               cout << "Your product is: " << a * b << endl;
-               break;
-           case '4':
-               cout << "Your quotient is: " << a / b << endl;
-               break;
-           case '5':
-               cout << "Your module is: " << a % b << endl;
-               break;
-           case '6':
-               temp = 1;
-               for (int i = 0; i < b; i++){
-               temp = a * temp;
-               }   
-               cout << "Your power is: " << temp << endl;
-               break;
+           case '1':                                                   // Case 1 is "Addition"
+               cout << "Your addition is: " << a + b << endl;          // The function that adds variables a and b together.
+               break;                                                  // Ends the case.
+           case '2':                                                   // Case 2 is "Subtraction"
+               cout << "Your subtraction is: " << a - b << endl;       // The function that subtracts variables a and b from each other. 
+               break;                                                  // Ends the case.
+           case '3':                                                   // Case 3 is "Multiplication"  
+               cout << "Your product is: " << a * b << endl;           // The function that multiplies variables a and b together.
+               break;                                                  // Ends the case.
+           case '4':                                                   // Case 4 is "Division"
+               cout << "Your quotient is: " << a / b << endl;          // The function that divides variable a by variable b.
+               break;                                                  // Ends the case.
+           case '5':                                                   // Case 5 is "Modulus"     
+               cout << "Your module is: " << a % b << endl;            // The function finds the remainder of variable a being divided by variable b.  
+               break;                                                  // Ends the case.
+           case '6':                                                   // Case 6 is "Power" 
+               temp = 1;                                               // The for loop is used here to simulate the power sign.
+               for (int i = 0; i < b; i++){                            // Variable b represents the amount of iterations of the for loop.    
+               temp = a * temp;                                        // Variable a is multiplied by 1 on the first iteration. On the second  
+               }                                                       // iteration variable a would be multiplied by itself, simulating a power sign.
+               cout << "Your power is: " << temp << endl;              // This prints the final temp variable.
+               break;                                                  // Ends the case.
 
-           default:   
-               cout << "Invalid operation" << endl;
-               break;
+           default:                                                    // Default happens when none of the cases (1 - 6) are selected. (Ex: 0 or 7)
+               cout << "Invalid operation" << endl;                    // This prints out "Invalid operation".  
+               break;                                                  // Ends the case.
        }
    return 0;
 }
