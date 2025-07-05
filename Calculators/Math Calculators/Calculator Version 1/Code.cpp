28d2f07d5edc02bb5cbd74b1fdfb409ca7ffa162
Sarah Rosales <sarahgrosales@gmail.com>
2025-07-05 02:50:50 -0400

    Version 1

--- Calculators/Math Calculators/Calculator Version 1/Code.cpp
+++ Calculators/Math Calculators/Calculator Version 1/Code.cpp
@@ -1,39 +1,41 @@
-#include <iostream>  //library
+// This is a basic calculator using functions. 
+
+#include <iostream>       //library
 using namespace std; 
 
-float x, y; 
+float x, y;               // Globally declares x and y as floats.
 
-void division(){
-	float quotient = x / y;
-	cout << “Quotient: “ << quotient <<end1; 
+void division(){					// This is the void division function.
+	float quotient = x / y;				// The equation to divide variables x and y.
+	cout << “Quotient: “ << quotient <<end1; 	// Prints the quotient.
 }
 
-void addition(){
-	float sum = x + y;
-	cout << “Sum: “ << sum <<end1; 
+void addition(){					// This is the void addition function.
+	float sum = x + y;				// The equation to add variables x and y.
+	cout << “Sum: “ << sum <<end1; 			// Prints the sum.
 }
 
-void multiplication(){
-	float product = x * y;
-	cout << “Product: “ << product <<end1; 
+void multiplication(){					// This is the void multiplication function.
+	float product = x * y;				// The equation to multiply variables x and y.
+	cout << “Product: “ << product <<end1; 		// Prints the product.
 }
 
-void subtraction(){
-	float difference = x - y;
-	cout << “Difference: “ << difference <<end1; 
+void subtraction(){					// This is the void subtraction function.
+	float difference = x - y;			// The equation to subtract variables x and y.
+	cout << “Difference: “ << difference <<end1; 	// Prints the difference.
 }
 
 int main(){ 
 
-	cout << “Enter the value of x: “;
-	cin >> x;
-	cout << “Enter the value of y: “;
-	cin >> y;
+	cout << “Enter the value of x: “;		// Prompts the user to enter the value of x.
+	cin >> x;					// The user enters the x value.
+	cout << “Enter the value of y: “;		// Prompts the user to enter the value of y.
+	cin >> y;					// The user enters the y value.
 	
-	division();
-  addition();
-  multiplication();
-	subtraction();
+	division();					// Calls the void division function.
+  	addition();					// Calls the void addition function.
+	multiplication();				// Calls the multiplication function.
+	subtraction();					// Calls the subtraction function.
 	
 	return 0;
 }
