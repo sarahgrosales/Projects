6319c04760bbb823f7c7b060c6831bce77094ba5
Sarah Rosales <sarahgrosales@gmail.com>
2026-05-22 17:52:59 -0400

    Create squares.py

---
+++ Python/squares.py
@@ -0,0 +1,9 @@
+# Print the table headings. 
+print('Number\tSquare') 
+print('--------------') 
+   
+# Print the numbers 1 through 10 
+# and their squares. 
+for number in range(1, 11): 
+    square = number**2 
+    print(f'{number}\t{square}') 
